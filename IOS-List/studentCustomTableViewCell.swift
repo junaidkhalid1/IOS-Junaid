b709b786f6482c3c816347d628f248c49635735e
Junaid KHALID <junaidkhalid@junaids-imac.local>
2017-01-18 20:42:49 +0100

    Initial Commit

---
+++ IOS-List/studentCustomTableViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  studentCustomTableViewCell.swift
+//  IOS-List
+//
+//  Created by Junaid KHALID on 18/01/17.
+//  Copyright © 2017 Junaid KHALID. All rights reserved.
+//
+
+import Foundation
---
+++ studentCustomCellViewTable.swift
@@ -0,0 +1,16 @@
+//
+//  studentCustomCellViewTable.swift
+//  IOS-List
+//
+//  Created by Junaid KHALID on 18/01/17.
+//  Copyright © 2017 Junaid KHALID. All rights reserved.
+//
+
+import UIKit
+
+class studentCustomCellViewTable: UITableViewCell {
+    @IBOutlet weak var cellImageView: UIImageView!
+
+    @IBOutlet weak var cellLabel1: UILabel!
+    @IBOutlet weak var cellLabel2: UILabel!
+}
